8aa9706ef749aabf432054c51be71d4fc6c30dd0
Vincent Pelletier <plr.vincent@gmail.com>
2014-01-24 18:01:33 +0100

    Bump to 1.2 .
    Add python version classifiers.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ioctl-opt',
     description="Functions to compute fnctl.ioctl's opt argument",
     keywords='ioctl',
-    version='1.1',
+    version='1.2',
     author='Vincent Pelletier',
     author_email='plr.vincent@gmail.com',
     url='http://github.com/vpelletier/python-ioctl-opt',
@@ -14,5 +14,7 @@ setup(
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
     ],
 )
