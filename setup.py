90fa037132e5ad3187e68367eb948297bae4778d
Vincent Pelletier <plr.vincent@gmail.com>
2018-03-21 13:39:52 +0000

    Bump to 1.2.2 .
    Now with long_description set, so doc appears on pypi.

--- setup.py
+++ setup.py
@@ -1,10 +1,31 @@
+# Copyright (C) 2016-2018  Vincent Pelletier <plr.vincent@gmail.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+from codecs import open
+import os
 from setuptools import setup, find_packages
 
 setup(
     name='ioctl-opt',
     description="Functions to compute fnctl.ioctl's opt argument",
+    long_description=open(
+        os.path.join(os.path.dirname(__file__), 'README.rst'),
+        encoding='utf8',
+    ).read(),
     keywords='ioctl',
-    version='1.2.1',
+    version='1.2.2',
     author='Vincent Pelletier',
     author_email='plr.vincent@gmail.com',
     url='http://github.com/vpelletier/python-ioctl-opt',
