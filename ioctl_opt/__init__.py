72065ea125f44ac994e185fa1df08e1a7cb1e69a
Martin Rys <martin@rys.rs>
2025-11-15 11:28:52 +0100

    PEP 257

--- ioctl_opt/__init__.py
+++ ioctl_opt/__init__.py
@@ -13,8 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-"""
-Pythonified linux asm-generic/ioctl.h .
+"""Pythonified linux asm-generic/ioctl.h
 
 Produce IOCTL command numbers from their individual components, simplifying
 C header conversion to python (keeping magic constants and differences to
